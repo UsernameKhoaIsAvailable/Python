75b3a0f58d4e19c6d497e009d914f16cbd4e6a90
Khoa <khoa@gmail.com>
2022-09-01 17:10:16 +0700

    refactor bai7

--- 4/bai7.py
+++ 4/bai7.py
@@ -1,65 +1,54 @@
 import os
 
-def choose_1(user_choose):
-    if user_choose == '1':
-        key = input('Key: ')
+def query():
+    key = input('Key: ')
 
-        try:
-            file = open(key, 'r')
-            print(file.read())
-            file.close()
-        except:
-            print('Không tìm thấy!')
+    try:
+        file = open('Data/' + key, 'r')
+        print(file.read())
+        file.close()
+    except:
+        print('Không tìm thấy!')
 
-def choose_2(user_choose):
-    if user_choose == '2':
-        key = input('Key: ')
-        value = input('Value: ')
+def add():
+    key = input('Key: ')
+    value = input('Value: ')
 
-        if os.path.exists(key):
-            print('Key đã tồn tại!')
-        else:
-            file = open(key, 'w')
-            file.write(value)
-            file.close()
-
-def choose_3(user_choose):
-    if user_choose == '3':
-        key = input('Key: ')
-
-        if os.path.exists(key):
-            os.remove(key)
-            print('Đã xóa!')
-        else:
-            print('Không tìm thấy!')
-
-def choose_4(user_choose):
-    if user_choose == '4':
-        key = input('Key: ')
-
-        if os.path.exists(key):
-            value = input('Value: ')
-            
-            file = open(key, 'w')
-            file.write(value)
-            file.close()
-        else:
-            print('Không tìm thấy!')
-
-def choose_5(user_choose):
-    if user_choose == '5':
-        return False
+    if os.path.exists('Data/' + key):
+        print('Key đã tồn tại!')
     else:
-        return True
+        file = open('Data/' + key, 'w')
+        file.write(value)
+        file.close()
 
-def other_choose(user_choose):
-    choose = ['1', '2', '3', '4', '5']
-    if user_choose not in choose:
-        os.system('cls')
+def delete():
+    key = input('Key: ')
 
-def main():    
-    _bool = True
-    while _bool :
+    if os.path.exists('Data/' + key):
+        os.remove('Data/' + key)
+        print('Đã xóa!')
+    else:
+        print('Không tìm thấy!')
+
+def edit():
+    key = input('Key: ')
+
+    if os.path.exists('Data/' + key):
+        value = input('Value: ')
+        
+        file = open('Data/' + key, 'w')
+        file.write(value)
+        file.close()
+    else:
+        print('Không tìm thấy!')
+
+def main():
+    try:
+        os.mkdir('Data')
+    except FileExistsError:
+        pass
+        
+    while True:
         print('1. Truy xuất dữ liệu')
         print('2. Thêm dữ liệu')
         print('3. Xóa dữ liệu')
@@ -67,13 +56,20 @@ def main():
         print('5. Thoát')
         user_choose = input('=> ')
 
-        choose_1(user_choose)
-        choose_2(user_choose)
-        choose_3(user_choose)
-        choose_4(user_choose)
-        _bool = choose_5(user_choose)
-        other_choose(user_choose)
-        continue
+        if user_choose == '1':
+            query()
+        elif user_choose == '2':
+            add()
+        elif user_choose == '3':
+            delete()
+        elif user_choose == '4':
+            edit()
+        elif user_choose == '5':
+            break
+        else:
+            os.system('cls')        
 
 if __name__ == '__main__':
     main()
+
+       
