d8b8f84ecfd8a5b89078254dd1a3f24f0334414f
Khoa <khoa@gmail.com>
2022-09-06 19:23:36 +0700

    fix LinkedList22

--- 6/LinkedList22.py
+++ 6/LinkedList22.py
@@ -1,7 +1,7 @@
 from linked_list import LinkedList
 class LinkedList22(LinkedList):
     def return_Kth_to_last(self, K):
-        if self.length < K:
+        if self.length < K or K < 0:
             raise Exception(f"Cannot return the {K}th to last element")
         index = self.length - K - 1
         current_node = self.head
@@ -9,7 +9,7 @@ class LinkedList22(LinkedList):
 
         while current_node != None:
             if i == index:
-                return current_node
+                return current_node.data
             current_node = current_node.next
             i += 1
        
@@ -27,4 +27,4 @@ class LinkedList22(LinkedList):
 # ex.add('qwerertyuio')
 # ex.add('qwerertyuio')
 # print(ex)
-# print(ex.return_Kth_to_last(7))
+# print(ex.return_Kth_to_last(56))
