915a9483d7d44fe0b262857df4d323723303b976
Khoa <khoa@gmail.com>
2022-09-06 20:59:11 +0700

    remake LinkedList23

--- 6/LinkedList23.py
+++ 6/LinkedList23.py
@@ -1,37 +1,34 @@
 from linked_list import LinkedList
 class LinkedList23(LinkedList):
-    def del_mid_node(self):
-        index = int(self.length / 2)
+    def del_mid_node(self, data):
+        current_node = self.head.next
+        prev_node = self.head
         
-        current_node = self.head
-        prev_node = None
-        i = 0
-
-        while current_node != None:
-            if i == index:
+        while current_node != self.tail:
+            if current_node.data == data:
                 prev_node.next = current_node.next
                 self.length -= 1
-                return
-            prev_node = current_node
-            current_node = current_node.next
-            i += 1
+            if prev_node.next.data != data:
+                prev_node = current_node
 
-# ex = LinkedList23()
-# ex.add('hello')
-# ex.add('hello')
-# ex.add('xinchao')
-# ex.add('tambiet')
-# ex.add('vinhbiet')
-# ex.add('hfdjhfjshhf')
-# ex.add('hfujhsfuhsfudhsfjdshf')
-# ex.add('hfueeif onvfnvfueiewnf0')
-# ex.add('qwerertyuio')
-# ex.add('qwerertyuio')
-# ex.add('qwerertyuio')
-# ex.add('qwerertyuio')
-# print(ex)
-# print(ex.length)
-# ex.del_mid_node()
-# print(ex)
-# print(ex.length)
+            current_node = current_node.next
+        
+ex = LinkedList23()
+ex.add('qwerertyuio')
+ex.add('qwerertyuio')
+ex.add('hello')
+ex.add('hello')
+ex.add('xinchao')
+ex.add('qwerertyuio')
+ex.add('qwerertyuio')
+ex.add('tambiet')
+ex.add('vinhbiet')
+ex.add('hfdjhfjshhf')
+ex.add('hfujhsfuhsfudhsfjdshf')
+ex.add('hfueeifonvfnvfueiewnf0')
+print(ex)
+print(ex.length)
+ex.del_mid_node('qwerertyuio')
+print(ex)
+print(ex.length)
 
