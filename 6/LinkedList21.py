b7ca27a4e81aeb49e4be66d4e26cb4f03deb21e6
Khoa <khoa@gmail.com>
2022-09-06 17:22:54 +0700

    edit remove_dups

--- 6/LinkedList21.py
+++ 6/LinkedList21.py
@@ -1,6 +1,6 @@
 from linked_list import LinkedList
 
-class LinkedList2(LinkedList):
+class LinkedList21(LinkedList):
     def remove_dups(self):
         current_node = self.head
         while current_node != None:
@@ -9,20 +9,28 @@ class LinkedList2(LinkedList):
             while dup_node != None:
                 if current_node.data == dup_node.data:
                     prev_dup_node.next = dup_node.next
+                    self.length -= 1
+
+                    if prev_dup_node.next == None:
+                        self.tail = prev_dup_node
                     return
                 else:
                     prev_dup_node = dup_node
                     dup_node = dup_node.next
             current_node = current_node.next
+    
 
 
-ex =LinkedList2()
-ex.add('xinchao')
+ex =LinkedList21()
 ex.add('hello')
+ex.add('hello')
+ex.add('xinchao')
 ex.add('tambiet')
 ex.add('vinhbiet')
-ex.add('hello')
 print(ex)
 ex.remove_dups()
 print(ex)
+print(ex.length)
+print(ex.head.data)
+print(ex.tail.data)
 
