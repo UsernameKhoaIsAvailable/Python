787ce16655795e8821282b97a166c52d65c80f10
Khoa <khoa@gmail.com>
2022-09-19 10:33:10 +0700

    rework stack_min

--- 7/stack_min.py
+++ 7/stack_min.py
@@ -1,34 +1,39 @@
 from stack import Stack
+
+
 class Stack32(Stack):
     def push(self, data):
         super().push(data)
-
-        if self.length == 1:
-            self.min = self.head.data
-        else:
-            if self.head.data < self.min:
-                self.min = self.head.data
-         
+        self.sorted_list.append(data)
+        self.sorted_list.sort(reverse=True)
 
     def __init__(self):
         super().__init__()
-        self.min = 0
+        self.sorted_list = []
 
     def minimum(self):
-        return self.min
+        return self.sorted_list[-1]
+
+    def pop(self):
+        self.sorted_list.remove(self.data_list.head.data)
+        return super().pop()
+
 
 def main():
     _list = Stack32()
     _list.push(2)
     _list.push(3)
-    _list.push(4)
-    _list.push(5)
     _list.push(6)
     _list.push(7)
-    _list.push(8)
+    _list.push(4)
+    _list.push(5)
     _list.push(1)
+    _list.push(8)
     print(_list)
-    print(_list.Minimum())
+    print(_list.minimum())
+    print(_list.pop())
+    print(_list.minimum())
+
 
 if __name__ == '__main__':
     main()
